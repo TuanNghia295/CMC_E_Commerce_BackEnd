f51a3131dfbcab44c27ded7a562b90f8995a0347
TuanNghia <tuannghiait2905@gmail.com>
2025-12-26 15:47:45 +0700

    Implement authentication request handling in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,19 @@ class ApplicationController < ActionController::API
 
   # Changes to the importmap will invalidate the etag for HTML responses
   # stale_when_importmap_changes
+
+  before_action :authenticate_request
+
+  attr_reader :current_user
+
+  private
+  def authenticate_request
+    header = request.headers["Authorization"]
+    token = header.split(" ").last if header
+
+    decoded = JwtService.decode(token)
+    @current_user = User.find(decoded[:user_id]) if decoded
+  rescue
+      render json: { error: "Unauthorized" }, status: :unauthorized
+  end
 end
