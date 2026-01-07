0158fe1708ea04161eb8a48f3b35c6879ccf5d24
TuanNghia <tuannghiait2905@gmail.com>
2026-01-07 11:13:44 +0700

    Update: CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "http://127.0.0.1:5173", "https://cmc-dashboard.netlify.app"
+    origins "*"
 
     resource "/api/v1/*",
       headers: :any,
