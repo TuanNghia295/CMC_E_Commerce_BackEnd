305eef109b45bf4aac8033817a3ca7fd0635437a
TuanNghia <tuannghiait2905@gmail.com>
2026-01-07 15:15:18 +0700

    Update: staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,25 @@
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local = true # để xem lỗi
+
+  config.consider_all_requests_local = true
+  config.action_controller.perform_caching = true
+
+  config.force_ssl = false
+
+  config.log_level = :debug
+  config.log_tags = [ :request_id ]
+
+  config.active_storage.service = :local
+
+  config.i18n.fallbacks = true
+  config.active_support.deprecation = :notify
+
+  config.log_formatter = ::Logger::Formatter.new
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
