d3198b1265df9f6b164e0cfe714442eee3110261
tuannghia2905 <tuannghiait2905@gmail.com>
2026-01-09 10:33:15 +0000

    Fix:  Def login conflicted with Soft Delete

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::AuthController < ApplicationController
 
   # POST /api/v1/auth/login
   def login
-    user = User.find_by(email: params[:email], deleted_at: nil?)
+    user = User.active.find_by(email: params[:email])
 
     if user&.authenticate(params[:password])
       access_token = JwtService.encode({ user_id: user.id })
